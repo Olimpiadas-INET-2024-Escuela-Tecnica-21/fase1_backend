73b6eff4e17ca8b027aa4b1589b1d02967fd46bb
Artec <its.javierjimenez@gmail.com>
2024-08-27 23:29:57 -0300

    Añadido clases repositorio y entidades
    clases repositorio para cada entidad se han logrado

--- src/entities/repository.js
+++ src/entities/repository.js
@@ -1,12 +1,17 @@
 import { PrismaClient } from "@prisma/client"; 
 
-const prisma = new PrismaClient();
+//const PRISMA = new PrismaClient();
 
 class Repository {
+    static #instance = undefined
+
     constructor() {
         if (new.target === Repository) {
             throw new Error('Cannot instantiate Repository class');
         }
+
+        //self.prisma = PRISMA;
+
     }
 
     sayHello() {
@@ -24,19 +29,118 @@ class Repository {
     async delete(){}
 }
 
-class UserRepository extends Repository {
+class ClientRepository extends Repository {
     static #instance = undefined
 
     constructor() {
         super();
 
-        if (!UserRepository.#instance) {
-            console.log('UserRepository instance created');
-            UserRepository.#instance = this;
-            return UserRepository.#instance;
+        
+
+        if (!ClientRepository.#instance) {
+            console.log('ClientRepository instance created');
+            ClientRepository.#instance = this;
+            return ClientRepository.#instance;
         }
 
-        throw new Error('Cannot create another instance of UserRepository because it is a singleton');
+        throw new Error('Cannot create another instance of ClientRepository because it is a singleton');
         
     }
 }
+
+class OrderRepository extends Repository {
+    static #instance = undefined
+
+    constructor() {
+        super();
+
+        
+
+        if (!OrderRepository.#instance) {
+            console.log('OrderRepository instance created');
+            OrderRepository.#instance = this;
+            return OrderRepository.#instance;
+        }
+
+        throw new Error('Cannot create another instance of OrderRepository because it is a singleton');
+        
+    }
+}
+
+class ProductRepository extends Repository {
+    static #instance = undefined
+
+    constructor() {
+        super();
+
+        
+
+        if (!ProductRepository.#instance) {
+            console.log('ProductRepository instance created');
+            ProductRepository.#instance = this;
+            return ProductRepository.#instance;
+        }
+
+        throw new Error('Cannot create another instance of ProductRepository because it is a singleton');
+        
+    }
+}
+
+class ProfileRepository extends Repository {
+    static #instance = undefined
+
+    constructor() {
+        super();
+
+        
+
+        if (!ProfileRepository.#instance) {
+            console.log('ProfileRepository instance created');
+            ProfileRepository.#instance = this;
+            return ProfileRepository.#instance;
+        }
+
+        throw new Error('Cannot create another instance of ProfileRepository because it is a singleton');
+        
+    }
+}
+
+class SalesManagerRepository extends Repository {
+    static #instance = undefined
+
+    constructor() {
+        super();
+
+        
+
+        if (!SalesManagerRepository.#instance) {
+            console.log('SalesManagerRepository instance created');
+            SalesManagerRepository.#instance = this;
+            return SalesManagerRepository.#instance;
+        }
+
+        throw new Error('Cannot create another instance of SalesManagerRepository because it is a singleton');
+        
+    }
+}
+
+class SellerRepository extends Repository {
+    static #instance = undefined
+
+    constructor() {
+        super();
+        
+        
+
+        if (!SellerRepository.#instance) {
+            console.log('SellerRepository instance created');
+            SellerRepository.#instance = this;
+            return SellerRepository.#instance;
+        }
+
+        throw new Error('Cannot create another instance of SellerRepository because it is a singleton');
+        
+    }
+}
+
+export { ClientRepository, OrderRepository, ProductRepository, ProfileRepository, SalesManagerRepository, SellerRepository }
---
+++ src/entities/salesManager.js
@@ -0,0 +1,23 @@
+class SalesManager {
+
+    static async create(data) {
+        return
+    }
+
+    static async update(id, data){
+        return
+    }
+
+    static async delete(id){
+        return
+    }
+
+    static async findMany(){
+        return 
+    }
+    
+    static async findById(id){
+        return
+    }
+
+}
